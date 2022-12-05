2cb4bea3debfd737c98e2baf61c2e0415f216885
seheerst <seherst9@gmail.com>
2022-12-05 13:36:04 +0300

    feat: ui design complated

--- lib/todoApp.dart
+++ lib/todoApp.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:todolist_app/Widgets/title_widget.dart';
+import 'package:todolist_app/Widgets/todoListItem_widget.dart';
+import 'package:todolist_app/Widgets/toolbar_widget.dart';
+import 'package:todolist_app/models/todo_model.dart';
+import 'package:uuid/uuid.dart';
 
 class TodoApp extends StatelessWidget {
+
   TodoApp({Key? key}) : super(key: key);
 
   final newTodoController = TextEditingController();
+
+  List<TodoModel> allTodos = [
+    TodoModel(id: Uuid().v4(), description: 'Spora Git'),
+    TodoModel(id: Uuid().v4(), description: 'Alışveriş yap'),
+    TodoModel(id: Uuid().v4(), description: 'Anneni ara'),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,7 +31,10 @@ class TodoApp extends StatelessWidget {
                   const InputDecoration(labelText: 'Neler Yapacaksın Bugün ?'),
               onSubmitted: (newTodo) {},
             ),
-            const SizedBox(height: 20,)
+            const SizedBox(height: 20,),
+            const ToolbarWidget(),
+            for(var i =0; i< allTodos.length;i++)
+              Dismissible(key:ValueKey(allTodos[i].id),child: TodoListItemWidget(item: allTodos[i],))
           ],
         ),
       ),
