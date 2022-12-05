beeed2d963c4dfb9ef8fbfb7782de1187dcb1cf4
seheerst <seherst9@gmail.com>
2022-12-05 14:31:15 +0300

    feat: todo_list_manager created

---
+++ lib/providers/todo_list_manager.dart
@@ -0,0 +1,46 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:todolist_app/models/todo_model.dart';
+import 'package:uuid/uuid.dart';
+
+
+
+
+class TodoListManager extends StateNotifier<List<TodoModel>> {
+  TodoListManager([List<TodoModel>? initialTodos]) : super(initialTodos ?? []);
+
+  void addTodo(String description) {
+    var eklenecekTodo = TodoModel(id: Uuid().v4(), description: description);
+    state = [...state, eklenecekTodo];
+  }
+
+  void toggle(String id) {
+    state = [
+      for (var todo in state)
+        if (todo.id == id)
+          TodoModel(
+              id: todo.id,
+              description: todo.description,
+              complated: !todo.complated)
+        else
+          todo,
+    ];
+  }
+
+  void edit({required String id, required String newDescription}) {
+    state = [
+      for (var todo in state)
+        if (todo.id == id)
+          TodoModel(
+              id: todo.id,
+              description: newDescription,
+              complated: todo.complated)
+        else
+          todo
+    ];
+  }
+
+  void remove(TodoModel silinecekTodo) {
+    state = state.where((element) => element.id != silinecekTodo.id).toList();
+  }
+}
