b9d9c512906c777cf169c5ffef3f066dda30d25d
seheerst <seherst9@gmail.com>
2022-12-09 17:44:31 +0300

    feat: filteredTodoList provider added

--- lib/providers/all_providers.dart
+++ lib/providers/all_providers.dart
@@ -4,12 +4,33 @@ import 'package:uuid/uuid.dart';
 
 import '../models/todo_model.dart';
 
+enum TodoListFilter{
+  all,active,complated
+}
+
+final todoListFilter = StateProvider((ref)=> TodoListFilter.all);
+
 final todoListProvider = StateNotifierProvider<TodoListManager,List<TodoModel>>((ref){
   return TodoListManager([
     TodoModel(id: Uuid().v4(), description: 'Spora git'),
     TodoModel(id: Uuid().v4(), description: 'Alışveriş yap'),
     TodoModel(id: Uuid().v4(), description: 'Ders çalış'),
   ]);
+});
+
+final filteredTodoList = Provider<List<TodoModel>>((ref){
+  final filter = ref.watch(todoListFilter);
+  final todoList = ref.watch(todoListProvider);
+
+  switch(filter){
+    case TodoListFilter.all:
+      return todoList;
+    case TodoListFilter.complated:
+      return todoList.where(((element) => element.complated)).toList();
+    case TodoListFilter.active:
+      return todoList.where((element) => !element.complated).toList();
+  }
+
 });
 
 final unComplatedTodoCount = Provider<int>((ref){
